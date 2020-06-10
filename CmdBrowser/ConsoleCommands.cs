2fcb2a7595fe429f74187857287010b1d010f866
GLProgrammer <jirka2002@gmail.com>
2020-06-10 21:53:40 +0200

    Commands added (ls, cd)
    Cd needs to be fixed

---
+++ CmdBrowser/ConsoleCommands.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CmdBrowser
+{
+    class ConsoleCommands
+    {
+        public static void ClearConsole(string path)
+        {
+            string username = Environment.UserName;
+            Console.Clear();
+            Console.WriteLine($"Welcome {username}! ");
+            WriteHeaderWithoutLine(path);
+        }
+
+        static void WriteHeaderWithoutLine(string path)
+        {
+            string username = Environment.UserName;
+            string computerName = Environment.UserDomainName;
+            if (path == $"C:\\Users\\{Environment.UserName}")
+                Console.Write($"{username}@{computerName}-> ~$ ");
+            else
+                Console.Write($"{username}@{computerName}-> {path}$ ");
+        }
+
+        public static void WriteHeader(string path)
+        {
+            Console.WriteLine();
+            string username = Environment.UserName;
+            string computerName = Environment.UserDomainName;
+            if (path == $"C:\\Users\\{Environment.UserName}")
+                Console.Write($"{username}@{computerName}-> ~$ ");
+            else
+                Console.Write($"{username}@{computerName}-> {path}$ ");
+        }
+
+        public static void ListDirectories(string path)
+        {
+            string[] directories = Directory.GetDirectories(path);
+            string[] files = Directory.GetFiles(path);
+
+            foreach (string directoryName in directories)
+            {
+                string directoryNameToWrite = directoryName.Replace(path + '\\', "");
+                if (directoryNameToWrite.StartsWith("."))
+                    Console.ForegroundColor = ConsoleColor.DarkBlue;
+                else
+                    Console.ForegroundColor = ConsoleColor.Blue;
+
+                Console.Write($" {directoryNameToWrite}/");
+            }
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Green;
+            foreach (string fileName in files)
+            {
+                string fileNameToWrite = fileName.Replace(path + '\\', "");
+                Console.Write($" {fileNameToWrite}");
+            }
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
+
+            WriteHeader(path);
+        }
+    }
+}
--- CmdBrowser/Program.cs
+++ CmdBrowser/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,57 @@ namespace CmdBrowser
 {
     class Program
     {
+        static string path = $"C:\\Users\\{Environment.UserName}";
         static void Main(string[] args)
         {
+            Console.Title = "CmdBrowser";
+            ConsoleCommands.ClearConsole(path);
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(' ');
+
+                switch (command[0])
+                {
+                    case "ls":
+                        ConsoleCommands.ListDirectories(path);
+                        break;
+
+                    case "cd":
+                        if(command.Length == 1)
+                        {
+                            path = $"C:\\Users\\{Environment.UserName}";
+                        }
+                        else
+                        {
+                            if (command[1] == "..")
+                            {
+                                string[] parsedPath = path.Split('\\');
+                                path = path.Replace("\\" + parsedPath[parsedPath.Length - 1], "");
+                            }
+                            else
+                            {
+                                if (Directory.Exists(path + "\\" + command[1]))
+                                    path += $"\\{command[1]}";
+                                else
+                                    Console.WriteLine("Path doesn´t exist!");
+                            }
+                        }
+                        
+
+                        ConsoleCommands.WriteHeader(path);
+                        break;
+
+                    default:
+                        Console.WriteLine("Command not found!");
+                        ConsoleCommands.WriteHeader(path);
+                        break;
+                }
+            }
+        }
+
+        public static string GetPath()
+        {
+            return path;
         }
     }
 }
